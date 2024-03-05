33f1bbb35c43827d23ef00d7482796fb219eb7aa
Chi-AnTai <andy.chian.tai@gmail.com>
2024-03-05 10:00:52 +0800

    feat: add TestERC20.sol

--- src/TestERC20.sol
+++ src/TestERC20.sol
@@ -2,35 +2,49 @@
 pragma solidity >=0.8.0;
 
 contract TestERC20 {
+    string public tokenName;
+    string public tokenSymbol;
+    uint256 public _totalSupply;
+    // address => A 允許
+    // address => B
+    // uint256 => 動用的錢
+    mapping(address => mapping(address => uint256)) public _allowance;
+    mapping(address => uint256) public _balance;
 
     constructor(string memory _name, string memory _symbol) {
+        tokenName = _name;
+        tokenSymbol = _symbol;
     }
 
     function mint(address to, uint256 amount) public {
-      
+        require(to != address(0));
+        _balance[to] += amount;
+        _totalSupply += amount;
     }
 
     function burn(address from, uint256 amount) public {
-      
+        require(from != address(0));
+        _balance[from] -= amount;
+        _totalSupply -= amount;
     }
 
     function name() public returns (string memory) {
-
+        return tokenName;
     }
 
     function symbol() public returns (string memory) {
-
+        return tokenSymbol;
     }
 
     function totalSupply() external view returns (uint256) {
-      
+      return _totalSupply;
     }
 
     /**
      * @dev Returns the value of tokens owned by `account`.
      */
     function balanceOf(address account) external view returns (uint256) {
-      
+      return _balance[account];
     }
 
     /**
@@ -41,7 +55,10 @@ contract TestERC20 {
      * Emits a {Transfer} event.
      */
     function transfer(address to, uint256 value) external returns (bool) {
-      
+        require(_balance[msg.sender] >= value);
+        require(to != address(0));
+        _balance[msg.sender] -= value;
+        _balance[to] += value;
     }
 
     /**
@@ -55,7 +72,7 @@ contract TestERC20 {
         address owner,
         address spender
     ) external view returns (uint256) {
-
+        return _allowance[owner][spender];
     }
 
     /**
@@ -74,7 +91,10 @@ contract TestERC20 {
      * Emits an {Approval} event.
      */
     function approve(address spender, uint256 value) external returns (bool) {
-      
+        require(spender != address(0));
+        require(msg.sender != address(0));
+        _allowance[msg.sender][spender] = value;
+        return true;
     }
 
     /**
@@ -91,6 +111,11 @@ contract TestERC20 {
         address to,
         uint256 value
     ) external returns (bool) {
-      
+        require(_allowance[from][msg.sender] >= value);
+        require(_balance[from] >= value);
+        _allowance[from][msg.sender] -= value;
+        _balance[from] -= value;
+        _balance[to] += value;
+        return true;
     }
 }
